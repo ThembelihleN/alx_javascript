9e06a52f805ccc404936ea0b3ab746bffccf97f3
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-04 21:30:34 +0200

    changed

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,11 +1,7 @@
-const add = function (a, b){
+/*const add = function (a, b){
+  return a + b;
+}*/
+
+const add = function(a, b) {
   return a + b;
 }
-
-
-
-/*const result = add(4, 85);
-function add(a, b) {
-  return a + b;
-}
-console.log(add); */
