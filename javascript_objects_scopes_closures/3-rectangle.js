7a767d29c5b9b90846a747483fe5c7235a42a145
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-07 06:48:41 +0200

    instance

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,9 @@
+class Rectangle{
+    constructor(w,h){
+        this.width = w
+        this.height = h
+    }
+}
+const instance = new Rectangle
+console.log(instance.emptyObject)
+//const emptyObject = Object.create(null);
