1f20d60baa8b1f7ae962c0d9cf473e467151b395
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-07 06:54:16 +0200

    methods

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -5,5 +5,7 @@ class Rectangle{
     }
 }
 const instance = new print(x)
+const newInstance = new rotate()
+const anotherInstance = new double(width * 2 ,height * 2)
 console.log(instance.emptyObject)
 //const emptyObject = Object.create(null);
