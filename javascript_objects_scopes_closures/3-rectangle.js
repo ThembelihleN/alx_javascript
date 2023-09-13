197fd9e47d75e89287b6486dba905f1f491e69cd
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-13 12:26:17 +0200

    third rectangle

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,17 @@
-#!/usr/bin/node
+//#!/usr/bin/node
 //Another rectangle class
 
 class Rectangle{
     constructor(w,h){
-        this.width = w
-        this.height = h
+        if (w > 0 && h > 0){
+            this.width = w;
+            this.height = h;
+        }
     }
 }
-if (w || h <= 0){
-    const emptyObject = Object.create(null);
-}
 
-const instance = new print(x)
-console.log(instance.emptyObject)
+const instance = new print("x")
+
 
 //module for exportation
 module.exports = Rectangle
