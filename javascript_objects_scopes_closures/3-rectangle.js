f1eaca3d1e21c7144d798e89140d5e24df5e0f0f
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-14 13:26:24 +0200

    modified

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,43 @@
-//#!/usr/bin/node
+#!/usr/bin/node
+module.exports = class Rectangle {
+    constructor (w, h) {
+      if ((w > 0) && (h > 0)) {
+        [this.width, this.height] = [w, h];
+      }
+    }
+    print () {
+      let sizeY = this.height;
+      while (sizeY > 0) {
+        console.log('X'.repeat(this.width));
+        sizeY -= 1;
+      }
+    }
+  };
+
+//module for exportation
+module.exports = Rectangle
+
+
+/*//#!/usr/bin/node
 //Another rectangle class
 
 class Rectangle{
     constructor(w,h){
-        let sizeY = this.height;
-            while (sizeY > 0) {
-                console.log('X'.repeat(this.width));
-                sizeY -= 1;
-            if ( w > 0 && h > 0) {
-                this.width = w;
-                this.height = h;
+        if ( w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+
+            print(){
+                let sizeY = this.height;
+                while (sizeY > 0) {
+                    console.log('X'.repeat(this.width));
+                    sizeY -= 1;
+                }
+           
             }
         }
     }
 }
 
 //module for exportation
-module.exports = Rectangle
+module.exports = Rectangle*/
