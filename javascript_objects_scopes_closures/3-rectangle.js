59d124ad8062083627088cda1052d4dff2fe5066
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-14 13:16:53 +0200

    changed

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -3,14 +3,13 @@
 
 class Rectangle{
     constructor(w,h){
-        if ( w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-
-                let sizeY = this.height;
-                    while (sizeY > 0) {
-                        console.log('X'.repeat(this.width));
-                        sizeY -= 1;
+        let sizeY = this.height;
+            while (sizeY > 0) {
+                console.log('X'.repeat(this.width));
+                sizeY -= 1;
+            if ( w > 0 && h > 0) {
+                this.width = w;
+                this.height = h;
             }
         }
     }
