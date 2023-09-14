c166c2bed98baed952ef5e9a26a1ed43cbf8e692
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-14 09:52:11 +0200

    first commit

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,7 @@
+//A script that displays the status code of a GET request
+
+const req =  require('request');
+req.get('https://intranet.alxswe.com/status', {encoding: 'utf-8'})
+.on('response', response => {
+    console.log(response.statusCode);
+})
