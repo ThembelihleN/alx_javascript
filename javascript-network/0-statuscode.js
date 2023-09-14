a2f6fdb840ced6a6e6206171a86717acf8996b23
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-14 10:24:27 +0200

    status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -3,5 +3,14 @@
 const req =  require('request');
 req.get('https://intranet.alxswe.com/status', {encoding: 'utf-8'})
 .on('response', response => {
-    console.log("code:" + response.statusCode);
+    console.log("code: " + response.statusCode);
 })
+
+req.get('https://intranet.alxswe.com/doesnt_exist', {encoding: 'utf-8'})
+.on('response', response => {
+    console.log("code: " + response.statusCode);
+})
+.catch(error => {
+    // Handle errors
+    console.error(error);
+  });
