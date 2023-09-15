d16e56c4ee2d25262c5996e58f15faadbd2ef8c9
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-15 20:19:01 +0200

    Complete

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -7,7 +7,7 @@ request(url, function (error, response){
     if (error){
         console.error(error);
 }
-    console.log('code: ', response + response.statusCode);
+    console.log('code: ' + response.statusCode);
 
 });
 
