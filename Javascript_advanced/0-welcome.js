b8bbca742e77f3581eda793ff7636177056cebed
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-18 13:13:54 +0200

    first commit

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -1,9 +1,9 @@
 //This is lexical scoping
 
 function welcome(firstName, lastName){
-    fullName = firstName," ",lastName;
+    fullName = firstName, lastName;
     function displayFullName(){
-        alert(welcome," ",fullName,"!");
+        alert("welcome " + fullName + "!");
     }
     displayFullName();
 }
