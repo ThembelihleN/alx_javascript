56fc84ced7a1f6903e9c5b6ac7beb6294120a2db
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-28 18:26:08 +0200

    store

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -5,4 +5,15 @@ const fs =  require('fs');
 
 const url = process.arg[2]
 
-request.get(url).pipe(fs.createWriteStream('response.html'))
+//request.get(url).pipe(fs.createWriteStream('response.html'))
+const file = process.argv[3];
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    }
+    fs.writeFile(file, body, 'utf-8', (error) => {
+        if (error) {
+            console.error('Error:', error);
+        }
+    })
+})
