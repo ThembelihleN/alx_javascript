b45135446f0625b1c1ee0e0d776c77307628278e
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-28 17:28:44 +0200

    modified

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,8 +1,7 @@
 //A script that displays the status code of a GET request
 
-const { error } = require('console');
 const request =  require('request');
-const url = process.arg[2]
+const url = process.arg[2];
 
 request.get(url, (error, response,body) => {
     if (error){
