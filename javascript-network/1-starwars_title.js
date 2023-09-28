8782e297da35bcde64a0b39aa1bfda9a427f16be
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-28 17:38:51 +0200

    changed

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,5 +1,6 @@
 //write a script that prints the title of a Star War movie where the episode number matches the given integer.
 
+/*
 const request = require('request');
 const url = process.arg[2]
 
@@ -8,4 +9,17 @@ request.get(url, (body))
     object.results.forEach(function (film){
        console.log(films.id);
     })
+*/
 
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    }
+    const jsonData = body;
+    const parsedData = JSON.parse(jsonData);
+    console.log(parsedData.title);
+})
