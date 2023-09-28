01c703fc190fa162c164b06b9cf26176b60a2225
Thembelihle <thembelihlengobese3@gmail.com>
2023-09-28 18:39:13 +0200

    characters

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,5 +1,23 @@
 //Write a script that prints all characters of a Star Wars movie
 
 const request = require("request");
-const url = process.argv[2]
+const movieId = process.argv[2]
 
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    }
+    const data = JSON.parse(body);
+    const allcharacters = data.characters;
+    for (const char of allcharacters) {
+        request.get(char, (error, response, charbody) => {
+            if (error) {
+                console.error('Error:', error);
+            } else {
+                const chardata = JSON.parse(charbody);
+                console.log(chardata.name);
+            }
+        })
+    }
+});
